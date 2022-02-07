8cf73d3037221eeaf42580b2ae65f8d30bba9cc0
Karen Gaspar <karenasgaspar@gmail.com>
2022-02-07 15:34:26 -0300

    To-Do-List início

---
+++ src/endpoints/createUser.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express'
+import insertUser from '../data/insertUser'
+
+
+export default async function createUser(
+    req: Request,
+    res: Response
+) {
+    try {
+        if (
+            !req.body.name || !req.body.nickname || !req.body.email
+        ) {
+            res.status(400).send('Please fill all the fields.')
+        }
+
+        const id = Date.now().toString()
+
+        await insertUser(
+            id,
+            req.body.name, 
+            req.body.nickname,
+            req.body.email
+        )
+            res.status(201).send('User created successfuly!')
+    } catch (error: any) {
+        res.status(400).send({
+            message: error.message || error.sqlMessage
+        })
+    }
+}
